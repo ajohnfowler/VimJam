d2c952db88230b6c28ae9512ad9fe44237752836
Adam Fowler <ajohnfowler@gmail.com>
2020-09-19 01:06:27 -0300

    Added new tileset

--- scripts/globals/map_generator.gd
+++ scripts/globals/map_generator.gd
@@ -1,20 +1,50 @@
 extends Node
 
-var map_lines = []
-var map_points = []
+var map_nodes = []
 var map_sections = []
-var there = []
-var back = []
+var number_of_steps = 4
+var max_node_gap
+
+class MapNode:
+	var position: Vector2
+	var next_node: MapNode
+	var previous_node: MapNode
+	var map_option: int
+	var passed: bool
+
+func _ready():
+	
+	max_node_gap = 1920/number_of_steps
+	randomize()
+	GenerateMap()
 
 func GenerateMap():
-	
 	MakePoints()
-	MakeLines()
 
+# Recusively create points from the starting point to end
 func MakePoints():
-	for x in range(0,3):
-		map_lines.append([x,x])
+	
+	# Make the There node
+	var there = MapNode.new()
+	there.position = Vector2(0,540) + Vector2(rand_range(100,200), rand_range(-300,300))
+	there.map_option = 1
+	there.passed = false
+	map_nodes.append(there)
+	
+	MakePoint(there, number_of_steps)
 
-func MakeLines():
+func MakePoint(parent, step_number):
+	if step_number <= 0:
+		return parent
+	
+	var node = MapNode.new()
+	node.position = parent.position + Vector2(rand_range(100,max_node_gap), rand_range(-300,300))
+
+	parent.next_node = node
+	map_nodes.append(node)
+	MakePoint(node, step_number-1)
+	return
+
+func MakeLines(start,end):
 	
 	pass
--- scripts/map.gd
+++ scripts/map.gd
@@ -1,11 +1,18 @@
 extends Node
 
-var map_tiles = []
+export (PackedScene) var map_point
 
 func _ready():
 	print (PlayerVariables.collected_maps)
-	# Generate map for testing here
-	MapGenerator.GenerateMap()
+	BuildMap()
+
+func BuildMap():
+	for node in MapGenerator.map_nodes:
+		var point = map_point.instance()
+		add_child(point)
+		point.position = node.position
+		if node.next_node != null:
+			point.get_node("Line").set_points([Vector2.ZERO, node.next_node.position-node.position])
 
 func _on_Button_pressed():
 	SceneManager.goto_scene("res://scenes/Game.tscn")
