e32a4342daf4c9f2bbb4f5c199252f371e19ec47
Adam Fowler <ajohnfowler@gmail.com>
2020-09-21 12:12:47 -0300

    Improved Menu layout

--- scripts/globals/player_variables.gd
+++ scripts/globals/player_variables.gd
@@ -2,17 +2,26 @@ extends Node
 class_name player_varaibles
 
 var collected_maps = []
-var abilities = []
+var collected_abilities = []
+
+var held_map
+var held_ability
 # Used to determine if player has reached "Back"
 # if so then all levels are reversed
 var been_to_there = false
 
 func CollectedMap(piece):
+	held_map = piece
 	collected_maps.append(piece)
 
 func PickedUpAbility(ability):
-	if not abilities.has(ability):
-		abilities.append(ability)
+	held_ability = ability
+	if not collected_abilities.has(ability):
+		collected_abilities.append(ability)
+
+func CompletedLevel():
+	collected_maps.append(held_map)
+	collected_abilities.append(held_ability)
 
 func ReachedThere():
 	been_to_there = true
