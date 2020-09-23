6d59f6514ed8c3362b1e5f15f0b4f947fe10d086
Adam Fowler <ajohnfowler@gmail.com>
2020-09-22 23:01:11 -0300

    Added menu to map

---
+++ scripts/gui/map_pause.gd
@@ -0,0 +1,29 @@
+extends Control
+class_name map_pause
+
+func _ready():
+	resume()
+	
+func pause():
+	get_tree().paused = true
+	$Panel.visible = true
+	
+func resume():
+	get_tree().paused = false
+	$Panel.visible = false
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
+
+func _on_Resume_pressed():
+	resume()
+
+func _on_Menu_pressed():
+	SceneManager.goto_scene("res://scenes/Menu.tscn")
+
+func _on_Quit_pressed():
+	get_tree().quit()
